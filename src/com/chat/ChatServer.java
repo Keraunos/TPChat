5573770b3892e5473f7decfcff28a09fb70d0c2f
Keraunos <gaetan.girin.eu@gmail.com>
2012-11-12 15:41:19 +0100

    Implemented connection to the server

--- src/com/chat/ChatServer.java
+++ src/com/chat/ChatServer.java
@@ -2,6 +2,7 @@ package com.chat;
 
 import java.rmi.*;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 
 /**
  * Server class for the chat
@@ -10,14 +11,36 @@ import java.rmi.server.UnicastRemoteObject;
  */
 public class ChatServer extends UnicastRemoteObject implements Chattable {
 
+    
+    public ArrayList<Message> messages;
+    public ArrayList<String> participants;
+    
+    
     public ChatServer() throws RemoteException {
         // TODO code
     };
     
+    
+    /**
+     * Connects the client to the server with id given as parameter
+     * 
+     * @param id the client id
+     */
     @Override
     public void connect(String id) {
-        // TODO code
+        
+        String msg;
+        
+        if (participants.contains(id)) {
+            msg = id + " is already connected.";
+        } else {
+            participants.add(id);
+            msg = "You are connected as " + id;
+        }
+        
+        System.out.println(msg);
     }
+    
 
     @Override
     public void send(String msg) {
