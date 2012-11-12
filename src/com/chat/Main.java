5a067b0d771e75734db0caf4976b0281bc5015fa
Keraunos <gaetan.girin.eu@gmail.com>
2012-11-12 15:04:34 +0100

    Initial commit

---
+++ src/com/chat/Main.java
@@ -0,0 +1,23 @@
+package com.chat;
+
+/**
+ * Documentation
+ * Chat Communication Protocole
+ * 
+ * Client interface:
+ * 
+ * connect <id> : connects client identified by <id> to the server
+ * send <msg> : sends to all participants the message <msg>
+ * bye : deconnects client from the server
+ * who : displays the list of currently connected clients
+ * receive : displays all the messages not yet displayed
+ * 
+ */
+
+/**
+ *
+ * @author gaetan
+ */
+public class Main {
+    
+}
