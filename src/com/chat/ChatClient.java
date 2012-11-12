c1358a5815ad8602a6793e60e4f9d6505868088c
Keraunos <gaetan.girin.eu@gmail.com>
2012-11-12 15:26:15 +0100

    Added chat client

---
+++ src/com/chat/ChatClient.java
@@ -0,0 +1,14 @@
+package com.chat;
+
+/**
+ * Client class for chat
+ * 
+ * @author gaetan
+ */
+public class ChatClient {
+    
+    public static void main(String args[]) {
+        // TODO code
+    }
+    
+}
