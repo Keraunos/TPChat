91881444a6f069907f4420c01f3c331869e4944e
Keraunos <gaetan.girin.eu@gmail.com>
2012-11-12 15:40:45 +0100

    Added message class

---
+++ src/com/chat/Message.java
@@ -0,0 +1,15 @@
+package com.chat;
+
+/**
+ * Message
+ * 
+ * @author gaetan
+ */
+class Message {
+   
+    public int id;
+    public String author_id;
+    public String content;
+    public String date;
+    
+}
