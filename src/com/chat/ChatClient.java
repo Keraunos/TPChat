58ed1567e25f4eeb78175f59f1ff26aacda60f9d
BMRagot <bastien.marichal@gmail.com>
2012-11-16 09:31:04 +0100

    Added a constructor to ChatClient and updated with the latest changes lost the last commit...

--- src/com/chat/ChatClient.java
+++ src/com/chat/ChatClient.java
@@ -1,5 +1,8 @@
 package com.chat;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.rmi.*;
 
 /**
@@ -8,6 +11,10 @@ import java.rmi.*;
  * @author gaetan
  */
 public class ChatClient {
+	
+	public ChatClient (){
+		
+	}
     
     public static void main(String args[]) {
         
@@ -16,7 +23,10 @@ public class ChatClient {
             // get stub on server object
             Chattable stub = (Chattable) Naming.lookup("//127.0.0.1:8080/chat_server");
             
-            System.out.println(stub.connect("client_0"));
+           // System.out.println(stub.connect("client_0"));
+            
+            ((ChatClient)stub).launchChat();
+
             
             
         } catch (Exception e) {
@@ -31,6 +41,23 @@ public class ChatClient {
         
         // TODO code
         
+    	 BufferedReader keyboard = new BufferedReader (new InputStreamReader (System.in));
+         try {
+ 			String cmd= keyboard.readLine();
+ 			String cmdparse[]= cmd.split("",2);
+ 			if(cmdparse[0].equals("connect")){
+ 	        	System.out.println(((Chattable)this).connect(cmdparse[1]));
+ 	        }
+ 	        	
+ 	        
+ 		} catch (IOException e) {
+ 			// TODO Auto-generated catch block
+ 			e.printStackTrace();
+ 		}
+
+    	
+    	
+    	
     };
     
 }
